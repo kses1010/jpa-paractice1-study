2cb332334ed715942dc14b303322ac5891e5d7b4
Sunny <thstkd92@gmail.com>
2020-08-10 17:40:39 +0900

    스프링 부트 실전 JPA 2. 회원도메인 기능 개발

---
+++ src/test/java/com/active/jpa/service/MemberServiceTest.java
@@ -0,0 +1,58 @@
+package com.active.jpa.service;
+
+import com.active.jpa.domain.Member;
+import com.active.jpa.repository.MemberRepository;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.transaction.annotation.Transactional;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.fail;
+
+@ExtendWith(SpringExtension.class)
+@SpringBootTest
+@Transactional
+class MemberServiceTest {
+
+    @Autowired
+    private MemberService memberService;
+
+    @Autowired
+    private MemberRepository memberRepository;
+
+    @Test
+    public void 회원가입() throws Exception {
+        //given
+        Member member = new Member();
+        member.setUsername("sunny");
+
+        //when
+        Long saveId = memberService.join(member);
+
+        //then
+        assertThat(member).isEqualTo(memberRepository.findOne(saveId));
+    }
+
+    @Test
+    public void 중복_회원_예외() throws Exception {
+        //given
+        Member member1 = new Member();
+        member1.setUsername("sunny");
+        Member member2 = new Member();
+        member2.setUsername("sunny");
+
+        //when
+        memberService.join(member1);
+        try {
+            memberService.join(member2);
+        } catch (IllegalStateException e) {
+            return;
+        }
+
+        //then
+        fail("예외가 발생한다.");
+    }
+}
